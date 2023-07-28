18d54bfd181adbe7cc04d45aa39969759318203a
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-07-27 22:53:07 -0600

    basic page
    created basic page with user inputs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function Channels({Platform}) {
+  const [addedChannels, setAddedChannels] = useState([]);
+  const [userInput, setUserInput] = useState('');
+
+  const handleUserInput = (e) => {
+    setUserInput(e.target.value);
+  }
+
+  const addChannel = () => {
+    let newChannels = addedChannels;
+    newChannels.push(userInput);
+    console.log(newChannels);
+    setUserInput('');
+    setAddedChannels([...newChannels]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div>platform: {Platform}</div>
+      <div>channels: {addedChannels}</div>
+      <form className="flex pt-1 gap-3">
+        <input
+          value={userInput}
+          onChange={handleUserInput}
+          placeholder="Channel"
+          className="bg-zinc-500"
+        />
+        <div onClick={addChannel} className="text-white bg-black p-2">add</div>
+      </form>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <div className="bg-blue-300 text-white h-screen flex flex-col gap-8">
+      <Channels Platform={'Twitch'}/>
+      <Channels Platform={'Youtube'}/>
+      <Channels Platform={'Kick'}/>
     </div>
   );
 }
-
-export default App;
